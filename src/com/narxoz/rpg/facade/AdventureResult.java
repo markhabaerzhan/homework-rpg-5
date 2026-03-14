3a5971b9c06fc2124572e9bb15d03f3e7915771f
markhabaerzhan <markhaba.erzhankyzy@narxoz.kz>
2026-03-15 02:28:56 +0500

    Add AdventureResult

--- src/com/narxoz/rpg/facade/AdventureResult.java
+++ src/com/narxoz/rpg/facade/AdventureResult.java
@@ -13,31 +13,24 @@ public class AdventureResult {
     public String getWinner() {
         return winner;
     }
-
     public void setWinner(String winner) {
         this.winner = winner;
     }
-
     public int getRounds() {
         return rounds;
     }
-
     public void setRounds(int rounds) {
         this.rounds = rounds;
     }
-
     public String getReward() {
         return reward;
     }
-
     public void setReward(String reward) {
         this.reward = reward;
     }
-
     public void addLine(String line) {
         log.add(line);
     }
-
     public List<String> getLog() {
         return Collections.unmodifiableList(log);
     }
